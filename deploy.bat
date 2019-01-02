f2111f6514975fe4628ef5ec518a6cdeeba92541
007slm <007slm@163.com>
2019-01-02 10:02:14 +0800

    add blog

---
+++ deploy.bat
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set SITE_SOURCE="g:/hugo/blog"
+set PUBLIC_DIR="g:/hugo/blog/public"
+cd %SITE_SOURCE%
+hugo -t jane
+git add .
+git commit -m "add blog"
+git pull
+git push
+cd %PUBLIC_DIR%
+git add .
+git commit -m "update public files"
+git push origin master --force
